f6ecd7e81747cf11ed5bb9c91b4b6e6ff43ec7ca
temikartemoff <outlooking@icloud.com>
2023-02-24 12:09:12 +0400

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(item => item.join(''));
+
+  while (bracketsConfig.length) {
+    let iterator = 0;
+
+    bracketsConfig.forEach(item => {
+      if (str.indexOf(item) != -1) {
+        str = str.replace(item, '');
+      } else {
+        iterator += 1;
+      }
+    });
+    
+    if (iterator === bracketsConfig.length) {
+      bracketsConfig.length = 0;
+    }
+  }
+  return str.length === 0 ? true : false;
 }
